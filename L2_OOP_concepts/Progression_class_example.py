0db3178351c916855debe97951fac53b6591fec1
akshay_mukundwar <akshaymukundwar82@gmail.com>
2020-06-01 07:41:56 -0400

    Oop_Concepts

--- L2_OOP_concepts/Progression_class_example.py
+++ L2_OOP_concepts/Progression_class_example.py
@@ -19,6 +19,19 @@ class Progression :
 
     def __next__(self):
         """Return the next element, or else raise StopIteration error"""
+        if self._current   is None :        # our convention to end a progression
+            raise StopIteration()
+        else:
+            answer =self._current           # record current value to return
+            self._advance()                 # advance to prepare for next time
+            return answer                   # return the answer
+    def __iter__(self):
+        """By convention, an iterator must return itself as an iterator."""
+        return self
+
+    def print_progression(self,n):
+        """Print next n values of the progression"""
+        print (''.join(str(next(self)) for j in range (n)))
 
 
 
